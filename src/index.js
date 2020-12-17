96a6cb352f38e0f7930308fd7f53eb4646533557
Vegar Norman <vegar.norman@kristiania.no>
2020-12-17 09:29:57 +0100

    Initial working Webpack setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello ma baby</h1>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
---
+++ webpack.config.dev.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/template.html',
+      title: 'Development'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
