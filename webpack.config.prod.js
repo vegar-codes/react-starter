16e1c97a8c060f146bf7b341d67a06f9738c0642
Vegar Norman <vegar.norman@kristiania.no>
2020-12-17 10:14:14 +0100

    Added production build process

---
+++ webpack.config.prod.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.js'
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/template.html',
+      title: 'Produksjon'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
