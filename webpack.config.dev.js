104fbb7a7470b9f91e9fbcdb6e7a5426dcfe596b
Vegar Norman <vegar.norman@kristiania.no>
2021-01-14 11:19:53 +0100

    Fix static URLs not working with React Router

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,7 +12,8 @@ module.exports = {
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
-    hot: true
+    hot: true,
+    historyApiFallback: true
   },
   plugins: [
     new Dotenv({
@@ -28,7 +29,8 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
