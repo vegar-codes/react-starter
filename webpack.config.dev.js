62670f1c9715671d9aa0b79c05bae7b20b183d85
Vegar Norman <vegar.norman@kristiania.no>
2021-01-18 16:25:33 +0100

    Updated to use CopyPlugin, updated config file structures, bumped version number

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,54 +3,62 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-    hot: true,
-    historyApiFallback: true
-  },
-  plugins: [
-    new Dotenv({
-      path: './.development.env'
-    }),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/template.html',
-      title: 'Development'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: ['@babel/plugin-transform-runtime']
-          }
-        }
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      index: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+      hot: true,
+      historyApiFallback: true
+    },
+    plugins: [
+      new Dotenv({
+        path: './.development.env'
+      }),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/template.html',
+        title: 'Development'
+      }),
+      new CopyPlugin({
+        patterns: [
+          {from: './other'}
+        ],
+      }),
+      new webpack.HotModuleReplacementPlugin()
     ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  }
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              plugins: ['@babel/plugin-transform-runtime']
+            }
+          }
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    }
+  };
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,48 +3,55 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: {
-    index: './src/index.js'
-  },
-  devtool: 'source-map',
-  plugins: [
-    new Dotenv({
-      path: './.production.env'
-    }),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/template.html',
-      title: 'Produksjon'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'build')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: ['@babel/plugin-transform-runtime']
-          }
-        }
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
+module.exports = () => {
+  return {
+    mode: 'production',
+    entry: {
+      index: './src/index.js'
+    },
+    devtool: 'source-map',
+    plugins: [
+      new Dotenv({
+        path: './.production.env'
+      }),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/template.html',
+        title: 'Produksjon'
+      }),
+      new CopyPlugin({
+        patterns: [
+          {from: './other'}
+        ],
+      })
     ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  }
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'build')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              plugins: ['@babel/plugin-transform-runtime']
+            }
+          }
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    }
+  };
 };
